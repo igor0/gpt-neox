288c48d811abd07785ad15ddc6a81b293c2d4356
Igor Ostrovsky <igoros@gmail.com>
2022-02-16 23:07:04 +0000

    Lens script

--- tools/lens.sh
+++ tools/lens.sh
@@ -1,18 +1,20 @@
-m=dense_small
 mode=extra_linear
 p=self
 MODELS_PATH=/mnt/ssd-1/igor/gpt-neox/models
 ROOT=~/igor/gpt-neox
 
-for j in {1..12}; do
-	for i in {0..12}; do
-		xm=$m.$mode.${p}_lens_$j.$i
-		cd $MODELS_PATH
-		$ROOT/tools/pythia/model_xform.py --predict $p --mode $mode --num_layers $i --head $m.$mode.$p.$j $m $xm || break
-		cd -
-		./tools/w.sh $xm || break
-		rm -rf $MODELS_PATH/$xm
+for m in dense_medium dense_large; do
+	for j in {0..24}; do
+		hm=$m.$mode.$p.$j
+		./tools/w.sh $hm || break
+
+		for i in {0..24}; do
+			xm=$m.$mode.${p}_lens_$j.$i
+			cd $MODELS_PATH
+			$ROOT/tools/pythia/model_xform.py --predict $p --mode $mode --num_layers $i --head $hm $m $xm || break
+			cd -
+			./tools/w.sh $xm || break 2
+			rm -rf $MODELS_PATH/$xm
+		done
 	done
 done
-
-
