ff094d213233d52d6ee084b09fc23a5a2159782e
Igor Ostrovsky <igoros@gmail.com>
2022-02-16 21:22:05 +0000

    Lens evaluation

--- tools/lens.sh
+++ tools/lens.sh
@@ -1,17 +1,17 @@
-m=dense_medium
+m=dense_small
 mode=extra_linear
 p=self
 MODELS_PATH=/mnt/ssd-1/igor/gpt-neox/models
 ROOT=~/igor/gpt-neox
 
-for j in 18 24 3 9 15 21; do
-	for i in {0..24}; do
+for j in {1..12}; do
+	for i in {0..12}; do
 		xm=$m.$mode.${p}_lens_$j.$i
 		cd $MODELS_PATH
 		$ROOT/tools/pythia/model_xform.py --predict $p --mode $mode --num_layers $i --head $m.$mode.$p.$j $m $xm || break
 		cd -
 		./tools/w.sh $xm || break
-		rm -rf $xm
+		rm -rf $MODELS_PATH/$xm
 	done
 done
 
