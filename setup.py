b3fe97307db10e47fdb2b1bd6687cc516ff21999
Igor Ostrovsky <igoros@gmail.com>
2022-03-30 22:01:55 +0000

    setup.py fixes

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+PKG_DIR = os.path.dirname(os.path.abspath(__file__))
+
 setuptools.setup(
     name="gpt-neox-igor",
-    version="0.0.1",
+    version="0.0.2",
     author="Igor Ostrovsky",
     author_email="igoros@gmail.com",
     description="GPT-NeoX (Igor's Version)",
@@ -15,7 +17,22 @@ setuptools.setup(
     project_urls={
         "Bug Tracker": "https://github.com/pypa/gpt-neox/issues",
     },
-    package_dir={"": "megatron"},
-    packages=setuptools.find_packages(where="megatron"),
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pybind11==2.6.2",
+        "six",
+        "regex",
+        "numpy",
+        "mpi4py==3.0.3",
+        "wandb==0.10.28",
+        "einops==0.3.0",
+        "transformers==4.5.0",
+        "tokenizers==0.10.2",
+        "lm_dataformat==0.0.19",
+        "ftfy==6.0.1",
+        "deepspeed @ git+https://github.com/EleutherAI/DeeperSpeed.git@eb7f5cff36678625d23db8a8fe78b4a93e5d2c75",
+        "lm-eval @ git+https://github.com/EleutherAI/lm-evaluation-harness.git@dc937d4b70af819c5695e09d94e59e4cdb1e40ad",
+        "fused_kernels @ file://localhost{}/megatron/fused_kernels".format(PKG_DIR),
+    ],
     python_requires=">=3.6",
 )
