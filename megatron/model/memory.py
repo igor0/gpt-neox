ad066f717b0d38ebbafa57b4e024b708bdff2b75
Igor Ostrovsky <igoros@gmail.com>
2022-04-12 16:32:42 +0000

    memory WIP

--- megatron/model/memory.py
+++ megatron/model/memory.py
@@ -6,6 +6,7 @@ class SimpleMemory:
         self.keys = None
         self.values = None
         self.eod_markers = None
+        self.first_token = None
 
     def add(self, keys, values, eod_markers):
         """
@@ -20,6 +21,7 @@ class SimpleMemory:
             self.keys = keys
             self.values = values
             self.valid_from = [0] * len(eod_markers)
+            self.first_token = [0] * len(eod_markers)
         else:
             self.keys = torch.cat((self.keys, keys), dim=0)
             self.values = torch.cat((self.values, values), dim=0)
@@ -30,6 +32,7 @@ class SimpleMemory:
             # if there are any EOD markers, invalidate the memories up to (but excluding) the last marker
             if eod_markers[i][0] <= eod_markers[i][1]:
                 self.valid_from[i] = self.keys.shape[0] - keys.shape[0] + eod_markers[i][1]
+                self.first_token[i] = self.keys.shape[0] - keys.shape[0]
 
         # drop some memories if we already have too much
 
@@ -41,6 +44,7 @@ class SimpleMemory:
 
             for i in range(len(eod_markers)):
                 self.valid_from[i] -= min(self.valid_from[i], removed_count)
+                self.first_token[i] -= removed_count
 
     def get(self, query_count, eod_markers):
         # Mask away:
@@ -53,10 +57,12 @@ class SimpleMemory:
             fill_value=True,
             device=self.keys.device)
 
+        print("keys", len(self.keys), self.valid_from)
         for batch in range(memory_mask.shape[0]):
             keys_valid_from = self.valid_from[batch]
             queries_valid_to = eod_markers[batch][0]
             memory_mask[batch][:][:queries_valid_to][keys_valid_from:] = False
+            memory_mask[batch][:][queries_valid_to:][:] = False
             #print(batch, "keys (", keys_valid_from, memory_mask.shape[3], ") queries (", 0, queries_valid_to, ")")
 
         return self.keys, self.values, memory_mask
